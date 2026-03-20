31eb6653f1d342ab517ae9048aa2735160cc7cb3
Ronal S. <ronal.sanx21@gmail.com>
2026-03-20 15:04:35 -0500

    Agregar gestión de tema con Zustand y componente BtnToggleTheme para alternar entre modos claro y oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { MyRoutes } from './routes/router';
+import { useThemeStore } from './store/ThemeStore';
 function App() {
   
   const queryClient = new QueryClient();
-
+  const {theme} = useThemeStore()
+  document.documentElement.classList.toggle("dark", theme === 'dark')
   return (
     <QueryClientProvider client={queryClient}>
       <MyRoutes/>
--- src/components/ui/button/BtnToggleTheme.jsx
+++ src/components/ui/button/BtnToggleTheme.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useThemeStore } from '../../../store/ThemeStore'
 
 export const BtnToggleTheme = () => {
-  return (
-    <button className='flex items-center gap-3 p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-primary/20 transition-all justify-center cursor-pointer'>
-        <span>Toggle Theme</span>
-    </button>
-  )
+
+    const {theme, setTheme} = useThemeStore()
+    return (
+        <button className='flex items-center gap-3 p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-primary/20 transition-all justify-center cursor-pointer' onClick={setTheme}>
+            <span>{theme === 'light' ? '🌙' : '☀️'}</span>
+            <span>{theme === 'light' ? 'Dark' : 'Light'}</span>
+        </button>
+    )
 }
