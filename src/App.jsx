004db8c98f792a0fde03d1eff7c871d6a02a365e
Ronal S. <ronal.sanx21@gmail.com>
2026-03-20 12:42:00 -0500

    Agregar el componente HomePage y configurar rutas en MyRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { MyRoutes } from './routes/router';
 function App() {
   
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <MyRoutes/>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
