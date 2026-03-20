52ee9bb7a9c0c22519636653ee12a6aa252f2c4e
Ronal S. <ronal.sanx21@gmail.com>
2026-03-20 12:38:52 -0500

    Agregar configuración de React Query en el componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 function App() {
   
+  const queryClient = new QueryClient();
 
   return (
-    <>
-  
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
